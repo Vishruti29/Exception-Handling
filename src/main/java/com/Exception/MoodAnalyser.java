2fcb200c4ea795b26f6bf63707e154aaf1ed8ee4
Vishruti Patil <vishruti2908@gmail.com>
2023-03-30 01:49:47 +0530

    Code to use constructor for happy and sad mood return and repeat TC1.1 and TC1.2

--- src/main/java/com/Exception/MoodAnalyser.java
+++ src/main/java/com/Exception/MoodAnalyser.java
@@ -1,14 +1,20 @@
 package com.Exception;
 public class MoodAnalyser {
         private String message;
+
+        public MoodAnalyser() {
+            this.message = "";
+        }
+
         public MoodAnalyser(String message) {
             this.message = message;
         }
-    public String analyseMood() {
-        if (message.contains("sad") || message.contains("Sad")) {
-            return "SAD";
-        } else {
-            return "HAPPY";
+
+        public String analyseMood() {
+            if (message.contains("sad") || message.contains("Sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
         }
-    }
 }
