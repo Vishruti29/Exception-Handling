bb878395638865a1b13cbcf5f41ddbb4da92465b
Vishruti Patil <vishruti2908@gmail.com>
2023-04-04 17:35:51 +0530

    Custom exception MoodAnalysisException added

--- src/main/java/com/Exception/MoodAnalyser.java
+++ src/main/java/com/Exception/MoodAnalyser.java
@@ -1,23 +1,24 @@
 package com.Exception;
+
 public class MoodAnalyser {
-    private String message;
-    public MoodAnalyser() {
-        this.message = "";
-    }
-    public MoodAnalyser(String message) {
-        this.message = message;
-    }
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-            if (message.isEmpty()) {
-                throw new MoodAnalysisException(MoodAnalysisErrorType.EMPTY_MESSAGE_ERROR.getMessage());
-            } else if (message.contains("sad") || message.contains("Sad")) {
-                return "SAD";
-            } else {
-                return "HAPPY";
-            }
-        } catch (NullPointerException e) {
-            throw new MoodAnalysisException(MoodAnalysisErrorType.NULL_MESSAGE_ERROR.getMessage());
+        private String message;
+        public MoodAnalyser() {
+            this.message = "";
         }
-    }
+        public MoodAnalyser(String message) {
+            this.message = message;
+        }
+        public String analyseMood() throws MoodAnalysisException {
+            try {
+                if (message.isEmpty()) {
+                    throw new MoodAnalysisException(MoodAnalysisErrorType.EMPTY_MESSAGE_ERROR.getMessage());
+                } else if (message.contains("sad") || message.contains("Sad")) {
+                    return "SAD";
+                } else {
+                    return "HAPPY";
+                }
+            } catch (NullPointerException e) {
+                throw new MoodAnalysisException(MoodAnalysisErrorType.NULL_MESSAGE_ERROR.getMessage());
+            }
+        }
 }
